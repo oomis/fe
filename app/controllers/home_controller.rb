51512181a1c902de32365ab584e4a476ec49d398
EC2 Default User <ec2-user@ip-172-31-9-58.us-east-2.compute.internal>
2019-05-15 17:18:03 +0000

    i dont know

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.all
-    @articles = Article.all
+    @posts = Post.all.order("created_at DESC")
+    @articles = Article.all.order("created_at DESC")
+    @categories = Category.all.order("created_at DESC")
+  end
+  
+  def show
+    @posts = Post.all.order("created_at DESC")
+    @articles = Article.all.order("created_at DESC")
+    @categories = Category.all.order("created_at DESC")
   end
 end
