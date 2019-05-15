334e213c5ec12ee362ffbfda926831e99639cefd
EC2 Default User <ec2-user@ip-172-31-9-58.us-east-2.compute.internal>
2019-05-15 10:35:00 +0000

    Fininshed category

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,17 +15,19 @@ class PostsController < ApplicationController
   # GET /posts/new
   def new
     @post = Post.new
+    @categories = Category.all.map{|c| [ c.name, c.id ] }
   end
 
   # GET /posts/1/edit
   def edit
+    @categories = Category.all.map{|c| [ c.name, c.id ] }
   end
 
   # POST /posts
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-
+    @product.category_id = params[:category_id]
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
@@ -40,6 +42,7 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
+    @post.category_id = params[:category_id]
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
     mount_uploaders :images, ImageUploader
     serialize :avatars, JSON
+    belongs_to :category
 end
